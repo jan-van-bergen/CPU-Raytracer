0879952f94244a6b0c300870037fa58ba8e79b74
jan <janvanbergen16@gmail.com>
2019-11-12 12:53:01 +0100

    Added libraries

--- Raytracer.cpp
+++ Raytracer.cpp
@@ -1,8 +1,6 @@
 // Raytracer.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-
 int main() {
-    std::cout << "Hello World!\n";
+	
 }
---
+++ include/ccd/config.h
@@ -0,0 +1,6 @@
+#ifndef __CCD_CONFIG_H__
+#define __CCD_CONFIG_H__
+
+#define CCD_SINGLE
+
+#endif /* __CCD_CONFIG_H__ */
