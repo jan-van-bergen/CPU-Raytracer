f89d0125fafa33485c636919042b7c3a77918a11
jan <janvanbergen16@gmail.com>
2019-11-14 18:03:07 +0100

    Matrix basics (probably needs more)

---
+++ Matrix4.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string.h>
+
+struct Matrix4 {
+	float cells[16];
+
+	inline Matrix4() {
+		memset(cells, 0, sizeof(cells));
+		cells[0]  = 1.0f;
+		cells[5]  = 1.0f;
+		cells[10] = 1.0f;
+		cells[15] = 1.0f;
+	}
+
+	inline       float & operator()(int i, int j)       { return cells[i + j << 2]; }
+	inline const float & operator()(int i, int j) const { return cells[i + j << 2]; }
+};
+
+inline Matrix4 operator*(const Matrix4 & left, const Matrix4 & right) {
+	Matrix4 result;
+
+	for (int j = 0; j < 4; j++) {
+		for (int i = 0; i < 4; i++) {
+			result(i, j) = 
+				left(i, 0) * right(0, j) +
+				left(i, 1) * right(1, j) +
+				left(i, 2) * right(2, j) +
+				left(i, 3) * right(3, j);
+		}
+	}
+
+	return result;
+}
