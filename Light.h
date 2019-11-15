d619d5e40b34456d255dc252b1dfcbbc2f62fe89
jan <janvanbergen16@gmail.com>
2019-11-15 13:42:26 +0100

    Better pow function

--- Light.h
+++ Light.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Math3d.h"
 #include "Vector3.h"
 
 struct Light {
@@ -8,21 +9,16 @@ struct Light {
 
 	// Calculate lighting using Blinn-Phong model
 	inline Vector3 calc_lighting(const Vector3 & normal, const Vector3 & to_light, const Vector3 & to_camera) const {
-		static const float specular_power = 128.0f;
+        float intensity = Vector3::dot(normal, to_light);
 
-		float diffuse_factor = Vector3::dot(normal, to_light);
-        float intensity = 0.0f;
+        if (intensity <= 0.0f) return Vector3(0.0f);
 
-        if (diffuse_factor > 0.0f) {
-            intensity = diffuse_factor;
+        Vector3 half_angle = Vector3::normalize(0.5f * (to_light + to_camera));
 
-            Vector3 half_angle = Vector3::normalize(0.5f * (to_light + to_camera));
-
-            float specular_factor = Vector3::dot(normal, half_angle);
-            if (specular_factor > 0.0f) {
-                intensity += powf(specular_factor, specular_power);
-			}
-        }
+        float specular_factor = Vector3::dot(normal, half_angle);
+        if (specular_factor > 0.0f) {
+            intensity += Math3d::pow2<128>(specular_factor);
+		}
 
         return intensity * colour;
 	}
--- Math3d.h
+++ Math3d.h
@@ -3,10 +3,24 @@
 
 namespace Math3d {
 	template<typename T>
-	T clamp(T value, T min, T max) {
+	inline T clamp(T value, T min, T max) {
 		if (value < min) return min;
 		if (value > max) return max;
 
 		return value;
 	}
+	
+	// Calculates N-th power by repeated squaring. This only works when N is a power of 2
+	template<int N> inline float pow2(float value);
+
+	template<>      inline float pow2<0>(float value) { return 1.0f; }
+	template<>      inline float pow2<1>(float value) { return value; }
+	template<int N> inline float pow2   (float value) { float sqrt = pow2<N / 2>(value); return sqrt * sqrt; }
+
+	// Calculates N-th power by repeated squaring. This only works when N is a power of 2
+	template<int N> inline double pow2(double value);
+
+	template<>      inline double pow2<0>(double value) { return 1.0; }
+	template<>      inline double pow2<1>(double value) { return value; }
+	template<int N> inline double pow2   (double value) { double sqrt = pow2<N / 2>(value); return sqrt * sqrt; }
 }
