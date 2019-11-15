7a1184beee6d92ff40d86cf352ef9819ef714063
jan <janvanbergen16@gmail.com>
2019-11-15 15:33:25 +0100

    Cleanup

--- Texture.h
+++ Texture.h
@@ -10,11 +10,11 @@ struct Texture {
 private:
 	unsigned * data;
 
-public:
 	int   width,   height;
 	float width_f, height_f;
 
-	static const Texture * load(const char * file_path);
-
+public:
 	Vector3 sample(float u, float v) const;
+
+	static const Texture * load(const char * file_path);
 };
