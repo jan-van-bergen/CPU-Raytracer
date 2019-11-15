fc6e93411ac5075cb35e718f230f00689d0a9d47
jan <janvanbergen16@gmail.com>
2019-11-15 10:15:03 +0100

    Cleanup and comments

--- Camera.cpp
+++ Camera.cpp
@@ -3,27 +3,28 @@
 #include <SDL2/SDL.h>
 
 void Camera::resize(int width, int height) {
-	float half_fov = 0.5f * fov;
-
 	float half_width  = 0.5f * width;
 	float half_height = 0.5f * height;
 
-	float d = half_width / tanf(half_fov);
+	// Distance to the viewing plane
+	float d = half_width / tanf(0.5f * fov);
 
+	// Initialize viewing pyramid vectors
 	top_left_corner = Vector3(-half_width, half_height, d);
-	u_axis = Vector3::normalize(Vector3(width,  0.0f,   0.0f));
-	v_axis = Vector3::normalize(Vector3(0.0f,  -height, 0.0f));
+	x_axis = Vector3::normalize(Vector3(width,  0.0f,   0.0f));
+	y_axis = Vector3::normalize(Vector3(0.0f,  -height, 0.0f));
 }
 
 Ray Camera::get_ray(float x, float y) const {
 	Ray ray;
 	ray.origin    = position;
-	ray.direction = Vector3::normalize(top_left_corner_rotated + x * u_axis_rotated + y * v_axis_rotated);
+	ray.direction = Vector3::normalize(top_left_corner_rotated + x * x_axis_rotated + y * y_axis_rotated);
 
 	return ray;
 }
 
 void Camera::update(float delta, const unsigned char * keys) {
+	// Move Camera around
 	const float MOVEMENT_SPEED = 10.0f;
 	const float ROTATION_SPEED = 2.0f;
 
@@ -44,7 +45,8 @@ void Camera::update(float delta, const unsigned char * keys) {
 	if (keys[SDL_SCANCODE_LEFT])  rotation = Quaternion::axis_angle(Vector3(0.0f, 1.0f, 0.0f), -ROTATION_SPEED * delta) * rotation;
 	if (keys[SDL_SCANCODE_RIGHT]) rotation = Quaternion::axis_angle(Vector3(0.0f, 1.0f, 0.0f), +ROTATION_SPEED * delta) * rotation;
 
+	// Transform view pyramid according to rotation
 	top_left_corner_rotated = rotation * top_left_corner;
-	u_axis_rotated          = rotation * u_axis;
-	v_axis_rotated          = rotation * v_axis;
+	x_axis_rotated          = rotation * x_axis;
+	y_axis_rotated          = rotation * y_axis;
 }
--- Camera.h
+++ Camera.h
@@ -11,12 +11,12 @@ public:
 	Vector3    position;	
 	Quaternion rotation;
 
-	float fov;
+	float fov; // Field of View in radians
 
 private:
 	Vector3 top_left_corner, top_left_corner_rotated;
-	Vector3 u_axis,          u_axis_rotated;
-	Vector3 v_axis,          v_axis_rotated;
+	Vector3 x_axis,          x_axis_rotated;
+	Vector3 y_axis,          y_axis_rotated;
 
 public:
 	inline Camera(float fov) : fov(DEG_TO_RAD(fov)) { }
