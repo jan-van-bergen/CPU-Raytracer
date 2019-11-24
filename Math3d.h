72e8d57f80242eb38c6801d5624c68107e3bea54
jan <janvanbergen16@gmail.com>
2019-11-24 17:02:49 +0100

    Improved robustness

--- Math3d.h
+++ Math3d.h
@@ -10,7 +10,7 @@ namespace Math3d {
 
 		return value;
 	}
-	
+
 	// Interpolate between a,b,c given barycentric coordinates u,v
 	template<typename T>
 	inline T barycentric(const T & a, const T & b, const T & c, float u, float v) {
@@ -24,18 +24,24 @@ namespace Math3d {
 	inline Vector3 refract(const Vector3 & vector, const Vector3 & normal, float eta, float cos_theta, float k) {
 		return Vector3::normalize(eta * vector + (eta * cos_theta - sqrtf(k)) * normal);
 	}
+	
+	inline constexpr bool is_power_of_two(int n) {
+		if (n == 0) return false;
 
+		return (n & (n - 1)) == 0;
+	}
+	
 	// Calculates N-th power by repeated squaring. This only works when N is a power of 2
 	template<int N> inline float pow2(float value);
 
 	template<>      inline float pow2<0>(float value) { return 1.0f; }
 	template<>      inline float pow2<1>(float value) { return value; }
-	template<int N> inline float pow2   (float value) { float sqrt = pow2<N / 2>(value); return sqrt * sqrt; }
+	template<int N> inline float pow2   (float value) { static_assert(is_power_of_two(N)); float sqrt = pow2<N / 2>(value); return sqrt * sqrt; }
 
 	// Calculates N-th power by repeated squaring. This only works when N is a power of 2
 	template<int N> inline double pow2(double value);
 
 	template<>      inline double pow2<0>(double value) { return 1.0; }
 	template<>      inline double pow2<1>(double value) { return value; }
-	template<int N> inline double pow2   (double value) { double sqrt = pow2<N / 2>(value); return sqrt * sqrt; }
+	template<int N> inline double pow2   (double value) { static_assert(is_power_of_two(N)); double sqrt = pow2<N / 2>(value); return sqrt * sqrt; }
 }
