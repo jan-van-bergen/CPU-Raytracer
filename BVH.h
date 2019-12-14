0cf3abfa32ba9378c4ac5aec50db8c4cc372a65e
jan <janvanbergen16@gmail.com>
2019-12-14 11:39:39 +0100

    Refactor: BVH construction is now seperate and factored into function better

--- BVH.h
+++ BVH.h
@@ -2,11 +2,14 @@
 #include <cassert>
 #include <algorithm>
 
-#include "AABB.h"
-
-#include "PrimitiveList.h"
+#include "BVHConstructors.h"
 
 #include "ScopedTimer.h"
+
+#define CONSTRUCT_MEDIAN 0 // Split using median Primitive along the longest axis
+#define CONSTRUCT_OBJECT 1 // Evaluate SAH for every Primtive to determine if we should split there
+
+#define CONSTRUCTION_STRATEGY CONSTRUCT_OBJECT
 
 #define TRAVERSE_BRUTE_FORCE  0 // Doesn't use the tree structure of the BVH, checks every Primitive for every Ray
 #define TRAVERSE_TREE_NAIVE   1 // Traverses the BVH in a naive way, always checking the left Node before the right Node
@@ -51,131 +54,6 @@ struct BVHNode {
 	};
 	int count; // Stores split axis in its 2 highest bits, count in its lowest 30 bits
 
-	inline int partition_median(const PrimitiveType * primitives, int * indices, int first_index, int index_count) const {
-		float max_axis_length = -INFINITY;
-		int min_split_dimension = -1;
-
-		// Check splits along all 3 dimensions
-		for (int dimension = 0; dimension < 3; dimension++) {
-			// Sort along current dimension
-			std::sort(indices + first_index, indices + first_index + index_count, [&](int a, int b) {
-				return primitives[a].get_position()[dimension] < primitives[b].get_position()[dimension];	
-			});
-
-			float min = primitives[indices[first_index                  ]].get_position()[dimension];
-			float max = primitives[indices[first_index + index_count - 1]].get_position()[dimension];
-
-			float axis_length = max - min;
-			if (axis_length > max_axis_length) {
-				max_axis_length = axis_length;
-				min_split_dimension = dimension;
-			}
-		}
-		
-		// Sort indices so that they are sorted along the dimension that we want to split in
-		// This is only required if we didn't split along z
-		if (min_split_dimension != 2) {
-			std::sort(indices + first_index, indices + first_index + index_count, [&](int a, int b) {
-				return primitives[a].get_position()[min_split_dimension] < primitives[b].get_position()[min_split_dimension];	
-			});
-		}
-
-		return first_index + (index_count >> 1);
-	}
-
-	inline int partition_sah(const PrimitiveType * primitives, int ** indices, int first_index, int index_count, float parent_cost, float * sah, int * temp, int & split_dimension) const {
-		float min_cost = INFINITY;
-		int   min_split_index     = -1;
-		int   min_split_dimension = -1;
-
-		// Check splits along all 3 dimensions
-		for (int dimension = 0; dimension < 3; dimension++) {
-			// First traverse left to right along the current dimension to evaluate first half of the SAH
-			AABB aabb_left;
-			aabb_left.min = Vector3(+INFINITY);
-			aabb_left.max = Vector3(-INFINITY);
-			for (int i = 0; i < index_count - 1; i++) {
-				aabb_left.expand(primitives[indices[dimension][first_index + i]].aabb);
-				
-				sah[i] = aabb_left.surface_area() * float(i + 1);
-			}
-
-			// Then traverse right to left along the current dimension to evaluate second half of the SAH
-			AABB aabb_right;
-			aabb_right.min = Vector3(+INFINITY);
-			aabb_right.max = Vector3(-INFINITY);
-			for (int i = index_count - 1; i > 0; i--) {
-				aabb_right.expand(primitives[indices[dimension][first_index + i]].aabb);
-
-				sah[i - 1] += aabb_right.surface_area() * float(index_count - i);
-			}
-
-			// Find the minimum of the SAH
-			for (int i = 0; i < index_count - 1; i++) {
-				float cost = sah[i];
-				if (cost < min_cost) {
-					min_cost = cost;
-					min_split_index = first_index + i + 1;
-					min_split_dimension = dimension;
-				}
-			}
-		}
-
-		if (min_split_index == -1) abort();
-
-		// Check SAH termination condition
-		if (min_cost >= parent_cost) return -1;
-
-		float split = primitives[indices[min_split_dimension][min_split_index]].get_position()[min_split_dimension];
-
-		// Sort indices so that they are sorted along the dimension that we want to split in
-		// This is only required if we didn't split along z
-		for (int dimension = 0; dimension < 3; dimension++) {
-			if (dimension != min_split_dimension) {
-				int left  = first_index;
-				int right = min_split_index;
-
-				for (int i = first_index; i < first_index + index_count; i++) {
-					bool goes_left = primitives[indices[dimension][i]].get_position()[min_split_dimension] < split;
-
-					if (primitives[indices[dimension][i]].get_position()[min_split_dimension] == split) {
-						// In case the current primitive has the same coordianate as the one we split on along the split dimension,
-						// We don't know whether the primitive should go left or right.
-						// In this case check all primitive indices on the left side of the split that 
-						// have the same split coordinate for equality with the current primitive index i
-
-						int j = min_split_index - 1;
-						// While we can go left and the left primitive has the same coordinate along the split dimension as the split itself
-						while (j >= 0 && primitives[indices[min_split_dimension][j]].get_position()[min_split_dimension] == split) {
-							if (indices[min_split_dimension][j] == indices[dimension][i]) {
-								goes_left = true;
-
-								break;
-							}
-
-							j--;
-						}
-					}
-
-					if (goes_left) {					
-						temp[left++] = indices[dimension][i];
-					} else {
-						temp[right++] = indices[dimension][i];
-					}
-				}
-
-				assert(left  == min_split_index);
-				assert(right == first_index + index_count);
-
-				memcpy(indices[dimension] + first_index, temp + first_index, index_count * sizeof(int));
-			}
-		}
-
-		split_dimension = (min_split_dimension + 1) << 30;
-
-		return min_split_index;
-	}
-
 	inline void subdivide(const PrimitiveType * primitives, int ** indices, BVHNode nodes[], int & node_index, int first_index, int index_count, float * sah, int * temp) {
 		aabb = calculate_bounds(primitives, indices[0], first_index, first_index + index_count);
 		
@@ -194,8 +72,11 @@ struct BVHNode {
 		float cost = aabb.surface_area() * float(index_count); 
 
 		int split_dimension;
-		int split_index = partition_sah(primitives, indices, first_index, index_count, cost, sah, temp, split_dimension);
-		//int split_index = partition_median(primitives, indices, first_index, index_count);
+#if CONSTRUCTION_STRATEGY == CONSTRUCT_MEDIAN
+		int split_index = BVHConstructors::partition_median(primitives, indices, first_index, index_count, temp, split_dimension);
+#elif CONSTRUCTION_STRATEGY == CONSTRUCT_OBJECT
+		int split_index = BVHConstructors::partition_object(primitives, indices, first_index, index_count, cost, sah, temp, split_dimension);
+#endif
 
 		if (split_index == -1) {
 			// Leaf Node, terminate recursion
---
+++ BVHConstructors.h
@@ -0,0 +1,149 @@
+#pragma once
+#include "AABB.h"
+
+namespace BVHConstructors {
+	template<typename PrimitiveType>
+	inline AABB calculate_bounds(const PrimitiveType * primitives, const int * indices, int first, int last) {
+		AABB aabb;
+		aabb.min = Vector3(+INFINITY);
+		aabb.max = Vector3(-INFINITY);
+
+		// Iterate over relevant Primitives
+		for (int i = first; i < last; i++) {
+			aabb.expand(primitives[indices[i]].aabb);
+		}
+
+		// Make sure the AABB is non-zero along every dimension
+		for (int d = 0; d < 3; d++) {
+			if (aabb.max[d] - aabb.min[d] < 0.001f) {
+				aabb.max[d] += 0.005f;
+			}
+		}
+
+		return aabb;
+	} 
+
+	template<typename PrimitiveType>
+	inline void split_indices(const PrimitiveType * primitives, int ** indices, int first_index, int index_count, int * temp, int split_dimension, int split_index, float split) {
+		for (int dimension = 0; dimension < 3; dimension++) {
+			if (dimension != split_dimension) {
+				int left  = first_index;
+				int right = split_index;
+
+				for (int i = first_index; i < first_index + index_count; i++) {
+					bool goes_left = primitives[indices[dimension][i]].get_position()[split_dimension] < split;
+
+					if (primitives[indices[dimension][i]].get_position()[split_dimension] == split) {
+						// In case the current primitive has the same coordianate as the one we split on along the split dimension,
+						// We don't know whether the primitive should go left or right.
+						// In this case check all primitive indices on the left side of the split that 
+						// have the same split coordinate for equality with the current primitive index i
+
+						int j = split_index - 1;
+						// While we can go left and the left primitive has the same coordinate along the split dimension as the split itself
+						while (j >= 0 && primitives[indices[split_dimension][j]].get_position()[split_dimension] == split) {
+							if (indices[split_dimension][j] == indices[dimension][i]) {
+								goes_left = true;
+
+								break;
+							}
+
+							j--;
+						}
+					}
+
+					if (goes_left) {					
+						temp[left++] = indices[dimension][i];
+					} else {
+						temp[right++] = indices[dimension][i];
+					}
+				}
+
+				assert(left  == split_index);
+				assert(right == first_index + index_count);
+
+				memcpy(indices[dimension] + first_index, temp + first_index, index_count * sizeof(int));
+			}
+		}
+	}
+
+	template<typename PrimitiveType>
+	inline int partition_median(const PrimitiveType * primitives, int ** indices, int first_index, int index_count, int * temp, int & split_dimension) {
+		float max_axis_length = -INFINITY;
+		int   max_axis_dimension = -1;
+
+		// Find longest dimension
+		for (int dimension = 0; dimension < 3; dimension++) {
+			float min = primitives[indices[dimension][first_index                  ]].get_position()[dimension];
+			float max = primitives[indices[dimension][first_index + index_count - 1]].get_position()[dimension];
+
+			float axis_length = max - min;
+			if (axis_length > max_axis_length) {
+				max_axis_length = axis_length;
+				max_axis_dimension = dimension;
+			}
+		}
+
+		int median_index = first_index + (index_count >> 1);
+		float split = primitives[indices[max_axis_dimension][median_index]].get_position()[max_axis_dimension];
+		
+		split_indices(primitives, indices, first_index, index_count, temp, max_axis_dimension, median_index, split);
+
+		split_dimension = (max_axis_dimension + 1) << 30;
+
+		return median_index;
+	}
+
+	template<typename PrimitiveType>
+	inline int partition_object(const PrimitiveType * primitives, int ** indices, int first_index, int index_count, float parent_cost, float * sah, int * temp, int & split_dimension) {
+		float min_cost = INFINITY;
+		int   min_split_index     = -1;
+		int   min_split_dimension = -1;
+
+		// Check splits along all 3 dimensions
+		for (int dimension = 0; dimension < 3; dimension++) {
+			// First traverse left to right along the current dimension to evaluate first half of the SAH
+			AABB aabb_left;
+			aabb_left.min = Vector3(+INFINITY);
+			aabb_left.max = Vector3(-INFINITY);
+			for (int i = 0; i < index_count - 1; i++) {
+				aabb_left.expand(primitives[indices[dimension][first_index + i]].aabb);
+				
+				sah[i] = aabb_left.surface_area() * float(i + 1);
+			}
+
+			// Then traverse right to left along the current dimension to evaluate second half of the SAH
+			AABB aabb_right;
+			aabb_right.min = Vector3(+INFINITY);
+			aabb_right.max = Vector3(-INFINITY);
+			for (int i = index_count - 1; i > 0; i--) {
+				aabb_right.expand(primitives[indices[dimension][first_index + i]].aabb);
+
+				sah[i - 1] += aabb_right.surface_area() * float(index_count - i);
+			}
+
+			// Find the minimum of the SAH
+			for (int i = 0; i < index_count - 1; i++) {
+				float cost = sah[i];
+				if (cost < min_cost) {
+					min_cost = cost;
+					min_split_index = first_index + i + 1;
+					min_split_dimension = dimension;
+				}
+			}
+		}
+
+		if (min_split_index == -1) abort();
+
+		// Check SAH termination condition
+		if (min_cost >= parent_cost) return -1;
+
+		float split = primitives[indices[min_split_dimension][min_split_index]].get_position()[min_split_dimension];
+
+		split_indices(primitives, indices, first_index, index_count, temp, min_split_dimension, min_split_index, split);
+
+		split_dimension = (min_split_dimension + 1) << 30;
+
+		return min_split_index;
+	}
+}
