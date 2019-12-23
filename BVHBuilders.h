e0b537935038f8f56b73eb9d3995699fe13e700c
jan <janvanbergen16@gmail.com>
2019-12-23 14:43:56 +0100

    Cleanup

--- BVHBuilders.h
+++ BVHBuilders.h
@@ -2,96 +2,7 @@
 
 #include "BVHPartitions.h"
 
-#define BVH_TRAVERSE_BRUTE_FORCE  0 // Doesn't use the tree structure of the BVH, checks every Primitive for every Ray
-#define BVH_TRAVERSE_TREE_NAIVE   1 // Traverses the BVH in a naive way, always checking the left Node before the right Node
-#define BVH_TRAVERSE_TREE_ORDERED 2 // Traverses the BVH based on the split axis and the direction of the Ray
-
-#define BVH_TRAVERSAL_STRATEGY BVH_TRAVERSE_TREE_ORDERED
-
-#define BVH_AXIS_X_BITS 0x40000000 // 01 00 zeroes...
-#define BVH_AXIS_Y_BITS 0x80000000 // 10 00 zeroes...
-#define BVH_AXIS_Z_BITS 0xc0000000 // 11 00 zeroes...
-#define BVH_AXIS_MASK   0xc0000000 // 11 00 zeroes...
-
-template<typename PrimitiveType>
-struct BVHNode {
-	AABB aabb;
-	union {  // A Node can either be a leaf or have children. A leaf Node means count = 0
-		int left;  // Left contains index of left child if the current Node is not a leaf Node
-		int first; // First constains index of first primtive if the current Node is a leaf Node
-	};
-	int count; // Stores split axis in its 2 highest bits, count in its lowest 30 bits
-
-	inline bool is_leaf() const {
-		return (count & (~BVH_AXIS_MASK)) > 0;
-	}
-
-	inline bool should_visit_left_first(const Ray & ray) const {
-#if BVH_TRAVERSAL_STRATEGY == BVH_TRAVERSE_TREE_NAIVE
-		return true;
-#elif BVH_TRAVERSAL_STRATEGY == BVH_TRAVERSE_TREE_ORDERED
-		switch (count & BVH_AXIS_MASK) {
-			case BVH_AXIS_X_BITS: return ray.direction.x[0] > 0.0f;
-			case BVH_AXIS_Y_BITS: return ray.direction.y[0] > 0.0f;
-			case BVH_AXIS_Z_BITS: return ray.direction.z[0] > 0.0f;
-
-			default: abort();
-		}
-#endif
-	}
-
-	inline void trace(const PrimitiveType * primitives, const int * indices, const BVHNode nodes[], const Ray & ray, RayHit & ray_hit, const Matrix4 & world, int step) const {
-		SIMD_float mask = aabb.intersect(ray, ray_hit.distance);
-		if (SIMD_float::all_false(mask)) return;
-
-		if (is_leaf()) {
-			for (int i = first; i < first + count; i++) {
-				primitives[indices[i]].trace(ray, ray_hit, world, step + count);
-			}
-		} else {
-			if (should_visit_left_first(ray)) {
-				// Visit left Node first, then visit right Node
-				nodes[left    ].trace(primitives, indices, nodes, ray, ray_hit, world, step + 1);
-				nodes[left + 1].trace(primitives, indices, nodes, ray, ray_hit, world, step + 1);
-			} else {
-				// Visit right Node first, then visit left Node
-				nodes[left + 1].trace(primitives, indices, nodes, ray, ray_hit, world, step + 1);
-				nodes[left    ].trace(primitives, indices, nodes, ray, ray_hit, world, step + 1);
-			}
-		}
-	}
-
-	inline SIMD_float intersect(const PrimitiveType * primitives, const int * indices, const BVHNode nodes[], const Ray & ray, SIMD_float max_distance) const {
-		SIMD_float mask = aabb.intersect(ray, max_distance);
-		if (SIMD_float::all_false(mask)) return mask;
-
-		if (is_leaf()) {
-			SIMD_float hit(0.0f);
-
-			for (int i = first; i < first + count; i++) {
-				hit = hit | primitives[indices[i]].intersect(ray, max_distance);
-
-				if (SIMD_float::all_true(hit)) return hit;
-			}
-
-			return hit;
-		} else {
-			if (should_visit_left_first(ray)) {
-				SIMD_float hit = nodes[left].intersect(primitives, indices, nodes, ray, max_distance);
-
-				if (SIMD_float::all_true(hit)) return hit;
-
-				return hit | nodes[left + 1].intersect(primitives, indices, nodes, ray, max_distance);
-			} else {
-				SIMD_float hit = nodes[left + 1].intersect(primitives, indices, nodes, ray, max_distance);
-
-				if (SIMD_float::all_true(hit)) return hit;
-
-				return hit | nodes[left].intersect(primitives, indices, nodes, ray, max_distance);
-			}
-		}
-	}
-};
+template<typename PrimitiveType> struct BVHNode; // Forward Declaration
 
 namespace BVHBuilders {
 	template<typename PrimitiveType>
@@ -134,7 +45,8 @@ namespace BVHBuilders {
 		build_bvh(nodes[node.left + 1], primitives, indices, nodes, node_index, first_index + n_left, n_right, sah, temp);
 	}
 
-	inline int build_sbvh(BVHNode<Triangle> & node, const Triangle * triangles, int * indices[3], BVHNode<Triangle> nodes[], int & node_index, int first_index, int index_count, float * sah, int * temp[2], float inv_root_surface_area, AABB node_aabb) {
+	template<typename PrimitiveType>
+	inline int build_sbvh(BVHNode<PrimitiveType> & node, const Triangle * triangles, int * indices[3], BVHNode<PrimitiveType> nodes[], int & node_index, int first_index, int index_count, float * sah, int * temp[2], float inv_root_surface_area, AABB node_aabb) {
 		node.aabb = node_aabb;
 
 		if (index_count < 3) {
