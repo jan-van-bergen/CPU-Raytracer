95809b8d5abcb3f133b590daf8de00011ec61e35
jan <janvanbergen16@gmail.com>
2020-02-18 11:52:57 +0100

    Removed unnecesary constructor call

--- AABB.cpp
+++ AABB.cpp
@@ -45,8 +45,8 @@ SIMD_float AABB::intersect(const Ray & ray, const SIMD_Vector3 & inv_direction, 
 	SIMD_Vector3 t_min = SIMD_Vector3::min(t0, t1);
 	SIMD_Vector3 t_max = SIMD_Vector3::max(t0, t1);
 	
-	SIMD_float t_near = SIMD_float::max(SIMD_float::max(SIMD_float(Ray::EPSILON), t_min.x), SIMD_float::max(t_min.y, t_min.z));
-	SIMD_float t_far  = SIMD_float::min(SIMD_float::min(max_distance,             t_max.x), SIMD_float::min(t_max.y, t_max.z));
+	SIMD_float t_near = SIMD_float::max(SIMD_float::max(Ray::EPSILON, t_min.x), SIMD_float::max(t_min.y, t_min.z));
+	SIMD_float t_far  = SIMD_float::min(SIMD_float::min(max_distance, t_max.x), SIMD_float::min(t_max.y, t_max.z));
 
 	return t_near < t_far;
 }
